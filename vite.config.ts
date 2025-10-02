0fca15a27bed7e4653a5b87036ee56096575b457
danielhep <dh@danielheppner.com>
2025-10-02 19:46:29 -0400

    add mode switch for subpath

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [tailwindcss(), react()],
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [tailwindcss(), react()],
+        base: mode === 'production' ? '/otp-fares-v2-viewer/' : '/',
+  }
 })
